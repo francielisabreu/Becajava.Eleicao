a5a08e6f03394a473393ef1671a0989b5c8d066e
Francieli Silva Abreu <fsilvaab@everis.com>
2020-11-03 17:28:41 -0300

    inclusao dos repositorios

---
+++ src/main/java/br/eleicao/app/repository/EleitorRepository.java
@@ -0,0 +1,10 @@
+package br.eleicao.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import br.eleicao.app.model.Eleitor;
+
+@Repository
+public interface EleitorRepository extends CrudRepository <Eleitor, Long> {
+
+}
---
+++ src/main/java/br/eleicao/app/repository/VotoRepository.java
@@ -0,0 +1,10 @@
+package br.eleicao.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import br.eleicao.app.model.Voto;
+
+@Repository
+public interface VotoRepository extends CrudRepository <Voto, Long>{
+
+}
